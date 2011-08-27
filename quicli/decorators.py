b76f098641890ca5cdb17f5c4e8067095f00e3bd
Kyle Alan Hale <kylealanhale@gmail.com>
2011-08-27 01:15:58 +0000

    quicli: renaming quicli.guts.run_program to quicli.guts.ParserAssembler._assemble, and making quicli.decorators.guts callable as a regular function, which calls the above _assemble class method.

---
+++ quicli/decorators.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+from .guts import ParserAssembler
+
+def run(*args, **kwargs):
+    run_on_import = False
+    __internal_frame__ = True  # Used to detect the calling script
+    generated_wrapper = None
+    
+    def wrapper():
+        def wrapper(func):
+            __internal_frame__ = True  # Used to detect the calling script
+            setattr(func, '_cl_parser', (args, kwargs))
+            ParserAssembler._assemble(run_on_import, func)
+            return func
+        return wrapper
+    if len(args) == 1 and len(kwargs) == 0 and hasattr(args[0], '__call__'):
+        # Wrapping a function
+        func = args[0]
+        args = list(args)[1:]
+        generated_wrapper = wrapper()(func)
+    elif not len(args) and not len(kwargs):
+        # Called directly
+        ParserAssembler._assemble(run_on_import)
+    else:
+        # Modifying behavior and then wrapping function
+        if len(args):
+            run_on_import = args[0]
+        if 'run_on_import' in kwargs:
+            run_on_import = kwargs['run_on_import']
+        generated_wrapper = wrapper()
+    
+    return generated_wrapper
+
+def sub(*args, **kwargs):
+    def wrapper():
+        def wrapper(func):
+            setattr(func, '_cl_subparser', (args, kwargs))
+            return func
+        return wrapper
+    if len(args) == 1 and len(kwargs) == 0 and hasattr(args[0], '__call__'):
+        func = args[0]
+        args = list(args)[1:]
+        return wrapper()(func)
+    else:
+        return wrapper()
+
+def argument(*args, **kwargs):
+    def wrapper(func):
+        if not hasattr(func, '_cl_arguments'):
+            setattr(func, '_cl_arguments', [])
+        func._cl_arguments.append((args, kwargs))
+        return func
+    return wrapper
